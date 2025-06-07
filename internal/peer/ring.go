c80ad2e6f8de1a1cc7779daf7c235a1357338a4e
Ricardo <ricardo@Ricardos-MacBook-Pro.local>
2025-06-07 16:25:02 -0400

    created ring.go data structure with methods handling concurrency

--- internal/peer/ring.go
+++ internal/peer/ring.go
@@ -1,5 +1,14 @@
 package peer
 
+import (
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+
+	"github.com/stathat/consistent"
+)
+
 type Ring struct {
 	self string
 	peers *consistent.Consistent
@@ -8,7 +17,7 @@ type Ring struct {
 func NewRing(self string, peerList []string) *Ring {
 	r := &Ring {
 		self: self,
-		peers: consistent.New()
+		peers: consistent.New(),
 	}
 
 	for _, p := range peerList {
@@ -17,4 +26,40 @@ func NewRing(self string, peerList []string) *Ring {
 		}
 	}
 	return r
+}
+
+func (r *Ring) GetNode(key string) string {
+	node, err := r.peers.Get(key)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return node
+}
+
+func (r *Ring) IsOwner(key string) bool {
+	return r.GetNode(key) == r.self
+}
+
+func (r *Ring) ProxyGet(key string) (string,error) {
+	node := r.GetNode(key)
+	resp, err := http.Get("http://" + node + "/get?key=" + url.QueryEscape(key))
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	return string(body), nil
+}
+
+func (r *Ring) ProxySet(key, value string) error {
+	node := r.GetNode(key)
+	resp, err := http.PostForm("http://"+node+"/set", url.Values{
+		"key": {key},
+		"value": {value},
+	})
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return nil
 }
